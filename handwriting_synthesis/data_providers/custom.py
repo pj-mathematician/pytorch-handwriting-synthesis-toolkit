b8464fd8c29aba4f8b0810baf4fbbc29213fc77e
Pj-Mathematician <55710610+pj-mathematician@users.noreply.github.com>
2022-10-12 19:44:03 +0530

    ADAB compatibility

--- handwriting_synthesis/data_providers/custom.py
+++ handwriting_synthesis/data_providers/custom.py
@@ -11,3 +11,32 @@ class MyProvider(Provider):
 
     def get_validation_data(self):
         raise NotImplementedError
+
+class ADABProvider(Provider):
+    name = 'ADAB'
+
+    def __init__(self, data_path = None, validation_size = 0.2):
+        if data_path is None:
+            data_path = '../ADAB_home'
+        
+        f = open(data_path+'/strokes.txt','r')
+        handwriting_strokes = eval(f.read())
+        f.close()
+        f = open(data_path+'/keys.txt','r')
+        transcript_keys = eval(f.read())
+        f.close()
+        
+        splitting_index = int(len(transcript_keys) * validation_size)
+
+        self.validation_strokes = handwriting_strokes[0:splitting_index]
+        self.training_strokes = handwriting_strokes[splitting_index:]
+        self.validation_keys = transcript_keys[0:splitting_index]
+        self.training_keys = transcript_keys[splitting_index:]
+
+    def get_training_data(self):
+        for handwriting, transcript in zip(self.training_strokes, self.training_keys):
+            yield handwriting, transcript
+
+    def get_validation_data(self):
+        for handwriting, transcript in zip(self.validation_strokes, self.validation_keys):
+            yield handwriting, transcript
